1704b3620ceb7bb4800df35a9803525ba6362044
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-28 07:55:13 -0700

    Add files via upload

---
+++ timezone.java
@@ -0,0 +1,56 @@
+// Marvish Chandra
+
+public static void givenTime(UTC){
+    alphaTZ = UTC + 1;
+    australianCDT = UTC + 1030;
+    austalianCST = UTC + 930;
+    acreTime = UTC - 5;
+    australianCT1 = UTC + 930;
+    australianCT2 = UTC + 1030;
+    australianCWST = UTC + 845;
+    arabiaDT = UTC + 4;
+    ADT = UTC - 3;
+    AEDT = UTC + 11;
+    AEST = UTC + 10;
+    AET1 = UTC + 10;
+    AET2 = UTC + 11;
+    AFT = UTC + 430;
+    AKDT = UTC - 8;
+    AKST = UTC - 9;
+    ALMT = UTC + 6;
+    amazonST = UTC - 3;
+    armeniaST = UTC - 5;
+    amazonT = UTC - 4;
+    armeniaT = UTC + 4;
+    atlanticStandard = UTC - 4;
+    alaskaStandard = UTC - 9;
+    brazilTime = UTC - 3;
+    centralStandard = UTC - 6;
+    centralEurotime = UTC + 1;
+    chileStandard = UTC - 4;
+    easternAfricaT = UTC + 3;
+    fijiSummer = UTC + 13;
+    fijiStandard = UTC + 12;
+    galapagosTime = UTC - 6;
+    greenwichStandard = UTC + 0;
+    hongkongStandard = UTC + 8;
+    indiaStandard = UTC + 530;
+    irishStandard = UTC + 1;
+    koreaStandard = UTC + 9;
+    moscowDaylight = UTC + 4;
+    moscowStandard = UTC + 3;
+    nepalTime = UTC + 545;
+    philippineStandard = UTC + 8;
+    pakistanStandard = UTC + 5;
+    pyongyangTime = UTC + 830;
+    quebecTime = UTC - 4;
+    signaporeTime = UTC + 8;
+    turkeyTime = UTC + 3;
+    uruguayTime = UTC - 3;
+    chamorroStandard = UTC + 10;
+    easternStandard = UTC - 5;
+    mountainStandard = UTC - 6;
+    pacificStandard = UTC - 8;
+    hawaiiStandard = UTC - 9;
+    samoaStandard = UTC - 11;
+}
